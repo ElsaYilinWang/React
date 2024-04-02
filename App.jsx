88009816dc3fc29308ce62b02c73e0790ef90955
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-04-02 10:29:49 +0100

    React import/Export Practice

--- App.jsx
+++ App.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import Heading from "./Heading.jsx";
+import * as calculator from "./calculator.js";
 
 function App(){
     return <div>
         <Heading />
+        <ul>
+        <li>{calculator.add(1, 2)}</li>
+        <li>{calculator.multiply(2, 3)}</li>
+        <li>{calculator.subtract(7, 2)}</li>
+        <li>{calculator.divide(5, 2)}</li>
+    </ul>
     </div>
 }
